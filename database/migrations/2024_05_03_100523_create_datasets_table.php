5b178891928e4e018c311bea161ebf0fdb152d9f
marioapn3 <mario.aprilnino27@gmail.com>
2024-05-03 13:15:55 +0700

    ✨feature: db_migration_model

---
+++ database/migrations/2024_05_03_100523_create_datasets_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('datasets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('file_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('datasets');
+    }
+};
